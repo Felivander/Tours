802487fee6f7514db0aab4718979d205e4c8ae52
felivander <feli.vander@gmail.com>
2023-09-29 18:34:12 -0300

    uploading finished project

---
+++ src/Tour.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const Tour = ({ id, image, info, price, name , removeTours}) => {
+  //props communicate with other components
+
+  const [readMore, setReadMore] = useState(false);
+
+  const handleClick = () => {
+    setReadMore(!readMore);
+  };
+
+  return (
+    <article key={id} className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={handleClick}>{readMore ? 'close': 'read more'}</button>
+        </p>
+        <button onClick={()=>removeTours(id)} className="delete-btn">not interested</button>
+      </footer>
+    </article>
+  );
+};
+
+export default Tour;
